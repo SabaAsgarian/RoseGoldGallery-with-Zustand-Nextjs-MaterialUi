99ba718dafd03edcec3db399f89793eb5802ae20
Saba Asgarian <166124662+SabaAsgarian@users.noreply.github.com>
2024-09-11 21:01:03 +0330

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images:{
+        domains: ['https://parasteh.com/','https://cloud.parasteh.com','https://cloud.parasteh.com'],
+    }
+};
 
 export default nextConfig;
